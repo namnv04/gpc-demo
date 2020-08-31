987ad059026aa33050f0b6b3323cd3addd578b6b
Nam Vuong <namnv04@gmail.com>
2020-08-31 00:32:28 -0400

    Processing code in app component and unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,141 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { IRoute } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  const testInput = [
+    { from: 'A', to: 'B', distance: 5 },
+    { from: 'B', to: 'C', distance: 4 },
+    { from: 'C', to: 'D', distance: 8 },
+    { from: 'D', to: 'C', distance: 8 },
+    { from: 'D', to: 'E', distance: 6 },
+    { from: 'A', to: 'D', distance: 5 },
+    { from: 'C', to: 'E', distance: 2 },
+    { from: 'E', to: 'B', distance: 3 },
+    { from: 'A', to: 'E', distance: 7 },
+  ];
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'gpc-demo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('gpc-demo');
+  describe('process', () => {
+    it('works', () => {
+      const mockThis = {
+        getRouteDistance: (routeArray: IRoute[], route: string) => null,
+        getTripsByNumberOfStops: (
+          routeArray: IRoute[],
+          from: string,
+          to: string,
+          min: number,
+          max: number
+        ) => null,
+        getShortestDistance: (routeArray: IRoute[], from: string, to: string) =>
+          null,
+        getRoutesByDistanceLimit: (
+          routeArray: IRoute[],
+          from: string,
+          to: string,
+          limit: number
+        ) => null,
+      };
+      spyOn(mockThis, 'getRouteDistance');
+      spyOn(mockThis, 'getTripsByNumberOfStops');
+      spyOn(mockThis, 'getShortestDistance');
+      spyOn(mockThis, 'getRoutesByDistanceLimit');
+      const routes: IRoute[] = component.process.bind(mockThis)(
+        'AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7'
+      );
+      expect(routes).toEqual(testInput);
+      expect(mockThis.getRouteDistance).toHaveBeenCalledTimes(5);
+      expect(mockThis.getTripsByNumberOfStops).toHaveBeenCalledTimes(2);
+      expect(mockThis.getShortestDistance).toHaveBeenCalledTimes(2);
+      expect(mockThis.getRoutesByDistanceLimit).toHaveBeenCalledTimes(1);
+    });
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('gpc-demo app is running!');
+  describe('getRoutesByDistanceLimit', () => {
+    it('The number of different routes from C to C with a distance of less than 30', () => {
+      expect(
+        component.getRoutesByDistanceLimit(testInput, 'C', 'C', 30)
+      ).toEqual(5);
+    });
+  });
+
+  describe('getTripsByNumberOfStops', () => {
+    it('The number of trips starting at C and ending at C with a maximum of 3 stops', () => {
+      expect(
+        component.getTripsByNumberOfStops(testInput, 'C', 'C', 0, 3)
+      ).toEqual(2);
+    });
+    it('The number of trips starting at A and ending at C with exactly 4 stops', () => {
+      expect(
+        component.getTripsByNumberOfStops(testInput, 'A', 'C', 4, 4)
+      ).toEqual(2);
+    });
+    it('The number of trips starting at A and ending at C with exactly 4 stops', () => {
+      expect(component.getTripsByNumberOfStops(testInput, 'A', 'C', 4)).toEqual(
+        9
+      );
+    });
+  });
+
+  describe('getShortestDistance', () => {
+    it('Return shortest distance from A to C', () => {
+      expect(component.getShortestDistance(testInput, 'A', 'C')).toEqual(9);
+    });
+    it('Return shortest distance from A to E', () => {
+      expect(component.getShortestDistance(testInput, 'A', 'E')).toEqual(7);
+    });
+  });
+
+  describe('findRoutes', () => {
+    it('Return all trips for ABC', () => {
+      expect(component.findRoutes(testInput, ['A'], 'C')).toEqual([
+        'ABC',
+        'ADC',
+        'AEBC',
+        'ADEBC',
+        'ABCDC',
+        'ADCDEBC',
+        'ADCEBC',
+        'AEBCDC',
+        'ADEBCDC',
+      ]);
+    });
+    it('Return all trips for BB', () => {
+      expect(component.findRoutes(testInput, ['B'], 'B')).toEqual(['BCEB']);
+    });
+  });
+
+  describe('getRouteDistance()', () => {
+    it('Return 9 for route ABC', () => {
+      expect(component.getRouteDistance(testInput, 'ABC')).toEqual(9);
+    });
+    it('Return 5 for route AD', () => {
+      expect(component.getRouteDistance(testInput, 'AD')).toEqual(5);
+    });
+    it('Return 13 for route ADC', () => {
+      expect(component.getRouteDistance(testInput, 'ADC')).toEqual(13);
+    });
+    it('Return 22 for route AEBCD', () => {
+      expect(component.getRouteDistance(testInput, 'AEBCD')).toEqual(22);
+    });
+    it('Return 0 for route AED', () => {
+      expect(component.getRouteDistance(testInput, 'AED')).toEqual(0);
+    });
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,237 @@
 import { Component } from '@angular/core';
 
+export interface IRoute {
+  from: string;
+  to: string;
+  distance?: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'gpc-demo';
+  constructor() {}
+  inputData: string;
+  file: any;
+  output1: any;
+  output2: any;
+  output3: any;
+  output4: any;
+  output5: any;
+  output6: any;
+  output7: any;
+  output8: any;
+  output9: any;
+  output10: any;
+  submitted = false;
+
+  /**
+   * on user select a file
+   */
+  handleFileInput(e): void {
+    this.file = e.item(0);
+  }
+
+  /**
+   * Click submit and start processing
+   */
+  submit(): void {
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      this.inputData = fileReader.result.toString();
+      this.process(this.inputData);
+      this.submitted = true;
+    };
+    fileReader.readAsText(this.file);
+  }
+
+  /**
+   * Call individual methods and assign result to output props
+   * @param data string content from text file
+   */
+  process(data: string): IRoute[] {
+    const routeArray: IRoute[] = data.split(',').map((item) => {
+      item = item.trim();
+      const route: IRoute = {
+        from: item[0],
+        to: item[1],
+        distance: Number(item[2]),
+      };
+      return route;
+    });
+
+    this.output1 = this.getRouteDistance(routeArray, 'ABC');
+    this.output2 = this.getRouteDistance(routeArray, 'AD');
+    this.output3 = this.getRouteDistance(routeArray, 'ADC');
+    this.output4 = this.getRouteDistance(routeArray, 'AEBCD');
+    this.output5 = this.getRouteDistance(routeArray, 'AED');
+    this.output6 = this.getTripsByNumberOfStops(routeArray, 'C', 'C', 0, 3);
+    this.output7 = this.getTripsByNumberOfStops(routeArray, 'A', 'C', 4, 4);
+    this.output8 = this.getShortestDistance(routeArray, 'A', 'C');
+    this.output9 = this.getShortestDistance(routeArray, 'B', 'B');
+    this.output10 = this.getRoutesByDistanceLimit(routeArray, 'C', 'C', 30);
+    return routeArray;
+  }
+
+  /**
+   * Get all trips from two nodes that under max distance
+   * @param routeArray route data
+   * @param from one letter represent a from node
+   * @param to one letter repsent a to node
+   * @param maxDistance max distance
+   */
+  getRoutesByDistanceLimit(
+    routeArray: IRoute[],
+    from: string,
+    to: string,
+    maxDistance: number
+  ): any {
+    const allRoutes = this.findRoutes(routeArray, [from], to);
+    const routesWithDistance = allRoutes.map((r) => {
+      return {
+        route: r,
+        distance: this.getRouteDistance(routeArray, r),
+      };
+    });
+    const routesByLimit = routesWithDistance.filter(
+      (r) => r.distance < maxDistance
+    );
+    return routesByLimit.length;
+  }
+
+  /**
+   * Return all routes that has number of stops within a given range
+   * @param routeArray route data
+   * @param from one letter represent a from node
+   * @param to one letter repsent a to node
+   * @param minStops min number of stops
+   * @param maxStops max number of stops
+   */
+  getTripsByNumberOfStops(
+    routeArray: IRoute[],
+    from: string,
+    to: string,
+    minStops?: number,
+    maxStops?: number
+  ): any {
+    let allRoutes = this.findRoutes(routeArray, [from], to);
+    if (maxStops && minStops) {
+      allRoutes = allRoutes.filter(
+        (route) => route.length - 1 <= maxStops && route.length - 1 >= minStops
+      );
+    }
+    if (maxStops) {
+      allRoutes = allRoutes.filter((route) => route.length - 1 <= maxStops);
+    }
+    return allRoutes.length;
+  }
+
+  /**
+   * Return shortest distance of the trip of a given two nodes
+   * @param routeArray route data
+   * @param from one letter represent a from node
+   * @param to one letter repsent a to node
+   */
+  getShortestDistance(routeArray: IRoute[], from: string, to: string): any {
+    const allRoutes = this.findRoutes(routeArray, [from], to);
+    const routesWithDistance = allRoutes.map((r) =>
+      this.getRouteDistance(routeArray, r)
+    );
+    return Math.min(...routesWithDistance);
+  }
+
+  /**
+   * Supporting methods for findRoutes.
+   * @param routeStrings
+   * @param newRouteString
+   */
+  updateRouteStringsArray(
+    routeStrings: string[],
+    newRouteString: string
+  ): string[] {
+    let hasUpdated = false;
+    routeStrings.forEach((item: string, index) => {
+      if (newRouteString.includes(item) && !item.includes('_')) {
+        routeStrings[index] = newRouteString;
+        hasUpdated = true;
+      }
+    });
+    if (hasUpdated === false) {
+      routeStrings.push(newRouteString);
+    }
+    return routeStrings;
+  }
+
+  /**
+   * Return all routes from two given nodes
+   * @param routeArray route data
+   * @param routeStrings Array of route strings, initialized with the starting node value
+   * @param to one letter repsent a to node
+   */
+  findRoutes(routeArray: IRoute[], routeStrings: string[], to: string): any {
+    const currentTotal = routeStrings.length;
+    routeStrings.forEach((routeString) => {
+      const from = routeString[routeString.length - 1];
+      if (routeString.length > 1 && from === to) {
+        routeStrings = this.updateRouteStringsArray(
+          routeStrings,
+          routeString + '_'
+        );
+      }
+      routeArray.map((r) => {
+        if (r.from === from && routeString.indexOf(from + r.to) < 0) {
+          routeStrings = this.updateRouteStringsArray(
+            routeStrings,
+            routeString + r.to
+          );
+        }
+      });
+    });
+    // console.log(routeStrings);
+    let isDone = true;
+    let i = 0;
+    if (currentTotal === 1 || routeStrings.length > currentTotal) {
+      while (i < routeStrings.length) {
+        const lastNode = routeStrings[i][routeStrings[i].length - 1];
+        const firstNode = routeStrings[i][0];
+        if (lastNode !== to && lastNode !== firstNode) {
+          isDone = false;
+          break;
+        }
+        i++;
+      }
+    }
+
+    if (isDone === false) {
+      return this.findRoutes(routeArray, routeStrings, to);
+    } else {
+      return routeStrings
+        .filter((item: string) => item.includes('_'))
+        .map((item) => item.replace('_', ''));
+    }
+  }
+
+  /**
+   * Get distance of route
+   * @param data array route object
+   * @param route string represent a multi nodes routes
+   */
+  getRouteDistance(data: IRoute[], route: string): number {
+    const routeNodes = route.split('');
+    let totalDistance = 0;
+    let hasNoRoute = false;
+    const distance = routeNodes.reduce((prevValue, currentValue, index) => {
+      const found = data.find(
+        (r) => r.from === prevValue && r.to === currentValue
+      );
+      if (found) {
+        totalDistance += found.distance;
+      } else {
+        hasNoRoute = true;
+      }
+      return currentValue;
+    });
+    return hasNoRoute ? 0 : totalDistance;
+  }
 }
